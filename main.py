51cbd48c2591ca46c3b65695559fa7731ba05abe
unknown <maltang315@gmail.com>
2026-05-03 05:06:32 -0700

    Adding python File

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Our first Streamlit APP")
+st.subheader("Introducing Streamlit in Automating Everything with Python")
+st.write("""This is our first Web APP.
+Enjoy it!
+""")
